ce2e46f2b5b13cdb4c5eeb26c8f4d756fd828cef
Scott Nesham <snesham@facilisgroup.com>
2022-11-10 09:38:15 -0600

    remove redundant effect

--- src/shared/hooks/useForm.tsx
+++ src/shared/hooks/useForm.tsx
@@ -2,7 +2,7 @@ import { Children, useEffect, isValidElement, cloneElement } from "react";
 import { useRecoilCallback, useRecoilState } from "recoil";
 import { fieldSelector, fieldState, formSelector } from "../state";
 
-export const useForm = (formId, children) => {
+export const useForm = (formId) => {
   const [{ fields, fieldValues, getFieldsAndValues }, setForm] = useRecoilState(formSelector(formId));
   const resetForm = () => null;
 
@@ -10,19 +10,6 @@ export const useForm = (formId, children) => {
     console.log('do a submit');
     console.log({ fieldValues, getFieldsAndValues: await getFieldsAndValues() });
   }
-
-
-  useEffect(() => {
-    Children.forEach(children, (c: any) => {
-      setForm(c.type.displayName)
-      if (isValidElement(c)) {
-        return cloneElement(c, { formKey: formId } as any);
-      }
-
-      return c;
-    });
-
-  }, [children])
 
   return {
     resetForm,
